40752c2e8ff39195b31ff0f57eb418aee91bda8c
ygurwls <alvin20@naver.com>
2024-08-05 18:05:55 +0900

    240805
    CharacterPlayer Input system added

--- Source/NexusDefense/Game/NDGameMode.cpp
+++ Source/NexusDefense/Game/NDGameMode.cpp
@@ -5,15 +5,21 @@
 
 ANDGameMode::ANDGameMode()
 {
-	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
-	if (ThirdPersonClassRef.Class)
+	//static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	//if (ThirdPersonClassRef.Class)
+	//{
+	//	UE_LOG(LogTemp, Log, TEXT("문제없음"))
+	//	DefaultPawnClass = ThirdPersonClassRef.Class;
+	//}
+	//else
+	//{
+	//	UE_LOG(LogTemp, Log, TEXT("문제있음"))
+	//}
+
+	static ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Script/NexusDefense.NDCharacterPlayer"));
+	if (DefaultPawnClassRef.Class)
 	{
-		UE_LOG(LogTemp, Log, TEXT("문제없음"))
-		DefaultPawnClass = ThirdPersonClassRef.Class;
-	}
-	else
-	{
-		UE_LOG(LogTemp, Log, TEXT("문제있음"))
+		DefaultPawnClass = DefaultPawnClassRef.Class;
 	}
 
 	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerClassRef(TEXT("/Script/NexusDefense.NDPlayerController"));
