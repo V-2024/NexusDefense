3931c29a897a82e1361afd4b044db4a873d76e62
HyeokjinShin <114042791+ygurwls@users.noreply.github.com>
2024-10-15 17:21:19 +0900

    24-10-15

--- Source/NexusDefense/Animation/NDAnimInstance.cpp
+++ Source/NexusDefense/Animation/NDAnimInstance.cpp
@@ -5,69 +5,39 @@
 #include "GameFramework//Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
-UNDAnimInstance::UNDAnimInstance()
-{
-	MovingThreshold = 3.0f;
-	JumpingThreshold = 100.0f;
-	ActorForwardDirection = FVector::ForwardVector;
-	ActorRotation = FRotator::ZeroRotator;
-	CalculatedDirection = 0.0f;
-}
-
 void UNDAnimInstance::NativeInitializeAnimation()
 {
-	Super::NativeInitializeAnimation();
-	Owner = Cast<ACharacter>(GetOwningActor());
-	if (Owner)
+	if (Pawn == nullptr)
 	{
-		Movement = Owner->GetCharacterMovement();
-	}
-}
-
-void UNDAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
-{
-	Super::NativeUpdateAnimation(DeltaSeconds);
-
-	if (Movement)
-	{
-		Velocity = Movement->Velocity;
-		GroundSpeed = Velocity.Size2D();
-		bIsIdle = GroundSpeed < MovingThreshold;
-		bIsFalling = Movement->IsFalling();
-		bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshold);
-
-		// 1. GetOwningActor()를 사용한 전방 벡터
-		ActorForwardDirection = Owner->GetActorForwardVector();
-
-		// 2. GetActorRotation()을 사용한 회전
-		ActorRotation = Owner->GetActorRotation();
-
-		// 3. 수동으로 방향 계산
-		CalculatedDirection = CalculateDirection(Velocity, ActorRotation);
-	}
-}
-
-float UNDAnimInstance::CalculateDirection(const FVector& InVelocity, const FRotator& BaseRotation)
-{
-	if (InVelocity.SizeSquared() > KINDA_SMALL_NUMBER)
-	{
-		FMatrix RotMatrix = FRotationMatrix(BaseRotation);
-		FVector ForwardVector = RotMatrix.GetScaledAxis(EAxis::X);
-		FVector RightVector = RotMatrix.GetScaledAxis(EAxis::Y);
-		FVector NormalizedVel = InVelocity.GetSafeNormal2D();
-
-		// Get the angle between the velocity and the forward vector
-		float ForwardCosAngle = FVector::DotProduct(ForwardVector, NormalizedVel);
-		float ForwardDeltaAngle = FMath::Acos(ForwardCosAngle);
-
-		// Get the angle between the velocity and the right vector
-		float RightCosAngle = FVector::DotProduct(RightVector, NormalizedVel);
-		if (RightCosAngle < 0)
+		Pawn = TryGetPawnOwner();
+		if (Pawn)
 		{
-			ForwardDeltaAngle *= -1.f;
+			CharacterPlayer = Cast<ANDCharacterPlayer>(Pawn);
 		}
-
-		return FMath::RadiansToDegrees(ForwardDeltaAngle);
 	}
-	return 0.f;
+}
+
+void UNDAnimInstance::UpdateAnimationProperties()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+		if (Pawn)
+		{
+			CharacterPlayer = Cast<ANDCharacterPlayer>(Pawn);
+		}
+	}
+	if (Pawn)
+	{
+		FVector Speed = Pawn->GetVelocity();
+		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.f);
+		MovementSpeed = LateralSpeed.Size();
+
+		bIsInAir = Pawn->GetMovementComponent()->IsFalling();
+
+		if (CharacterPlayer == nullptr)
+		{
+			CharacterPlayer = Cast<ANDCharacterPlayer>(Pawn);
+		}
+	}
 }
--- Source/NexusDefense/Animation/NDAnimInstance.h
+++ Source/NexusDefense/Animation/NDAnimInstance.h
@@ -15,49 +15,20 @@ class NEXUSDEFENSE_API UNDAnimInstance : public UAnimInstance
 	GENERATED_BODY()
 
 public:
-	UNDAnimInstance();
-
-protected:
 	virtual void NativeInitializeAnimation() override;
 
-	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	UFUNCTION(BlueprintCallable, Category = AnimationProperties)
+	void UpdateAnimationProperties();
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
-	TObjectPtr<class ACharacter> Owner;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	float MovementSpeed;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
-	TObjectPtr<class UCharacterMovementComponent> Movement;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	bool bIsInAir;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	FVector Velocity;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	APawn* Pawn;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	float GroundSpeed;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	uint8 bIsIdle : 1;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	float MovingThreshold;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	uint8 bIsFalling : 1;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	uint8 bIsJumping : 1;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	float JumpingThreshold;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	FVector ActorForwardDirection;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	FRotator ActorRotation;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
-	float CalculatedDirection;
-
-private:
-	float CalculateDirection(const FVector& InVelocity, const FRotator& BaseRotation);
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	class ANDCharacterPlayer* CharacterPlayer;
 };
