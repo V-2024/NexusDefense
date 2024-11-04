a1d5fd7dc803d065c042b06326c67bf552067d17
HyeokjinShin <114042791+ygurwls@users.noreply.github.com>
2024-11-04 14:12:38 +0900

    241104

--- Source/NexusDefense/AI/NDAIWorker.h
+++ Source/NexusDefense/AI/NDAIWorker.h
@@ -27,12 +27,12 @@ public:
     bool GetCompletedTask(FNDAITask& OutTask);
 
 private:
-    FThreadSafeBool bStopThread;
-    FRunnableThread* Thread;
+    FThreadSafeBool bStopThread; // 스레드를 안전하게 멈추기 위한 변수
+    FRunnableThread* Thread; // 실행될 스레드
 
-    FCriticalSection TaskLock;
-    TQueue<FNDAITask> TaskQueue;
-    TQueue<FNDAITask> CompletedTasks;
+    FCriticalSection TaskLock; // 작업 목록을 안전하게 관리하기 위한 잠금장치
+    TQueue<FNDAITask> TaskQueue; // 해야 할 작업들을 보관하는 대기열
+    TQueue<FNDAITask> CompletedTasks; // 완료된 작업들을 보관하는 대기열
 
-    void ProcessTask(FNDAITask& Task);
+    void ProcessTask(FNDAITask& Task); // 작업을 처리하는 함수
 };
